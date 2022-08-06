6842ffc9f00d3c7e36b14920d32e293cfd08261f
m2rads <argo.mohrad@gmail.com>
2022-08-05 19:45:41 -0700

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
 import babel from "rollup-plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
+import { terser } from "rollup-plugin-terser";
+import postcss from "rollup-plugin-postcss";
 
 export default [
   {
     input: "./src/index.js",
     output: [
       {
-        //common js format
         file: "dist/index.js",
         format: "cjs",
       },
@@ -18,12 +19,17 @@ export default [
       },
     ],
     plugins: [
+      postcss({
+        plugins: [],
+        minimize: true,
+      }),
       babel({
         exclude: "node_modules/**",
         presets: ["@babel/preset-react"],
       }),
       external(),
       resolve(),
+      terser(),
     ],
   },
 ];
